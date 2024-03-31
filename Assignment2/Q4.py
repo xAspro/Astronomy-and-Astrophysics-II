eb7470d3c8618800314e2e186eaf5368fb956d2c
xAspro <sooryabalan.m@tifr.res.in>
2024-03-31 16:30:37 +0530

    Remodified Q4

--- Assignment2/Q4.py
+++ Assignment2/Q4.py
@@ -15,18 +15,18 @@ ni=50e-6
 ne=ni
 
 def alpha(nu,T,Z,ne,ni):
-    G = 1   #Gaunt factor
+    G=1   #Gaunt factor
     return (4*e**6/(3*m*h*c))*G*((2*np.pi)/(3*k*m))**0.5*(1/(T**0.5))*(Z**2*ne*ni/nu**3)*(1-np.exp(-h*nu/(k*T)))
 
-nu = np.linspace(10e8, 10e15, 1000) 
-temperatures = [1000, 1e5] 
-lengths = [10e5, 10e13] 
+nu=np.linspace(10e8,10e15,1000) 
+temperatures=[1000,1e5] 
+lengths=[10e5,10e13] 
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(10,6))
 
 for T in temperatures:
     for L in lengths:
-        tau = alpha(nu,T,1,ne,ni)*L
+        tau=alpha(nu,T,1,ne,ni)*L       #Taking Z=1
         plt.plot(nu,tau,label=f'Length={L}m,T={T}K')
 
 plt.xlabel('Frequency(Hz)')
